c43ba6bcfeb0a2e0c0fd0521d6075d55b2eb3b49
“PolarisAnnie” <polariseunhee@gmail.com>
2025-09-01 09:51:49 +0900

    feat: go router 및 하단 네비게이션 구현 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,124 +1,97 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_sns/theme/theme.dart';
+import 'package:flutter_sns/write/presentation/screens/home/home_page.dart';
+import 'package:flutter_sns/write/presentation/screens/post_detail/post_detail_page.dart';
+import 'package:flutter_sns/write/presentation/screens/profile/profile_edit_page.dart';
+import 'package:flutter_sns/write/presentation/screens/profile/profile_page.dart';
+import 'package:flutter_sns/write/presentation/screens/write_page.dart';
+import 'package:flutter_sns/write/presentation/widgets/bottom_navigation.dart';
+import 'package:go_router/go_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(); // Firebase 초기화
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application. 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp.router(
+      routerConfig: _router,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+// go_router 설정
+final GoRouter _router = GoRouter(
+  initialLocation: '/', // 홈 화면으로 시작되도록 설정
+  routes: [
+    // 메인 네비게이션 (하단 탭바)
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) {
+        return BottomNavigation(navigationShell: navigationShell);
+      },
+      branches: [
+        // 홈 탭
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/',
+              name: 'home',
+              builder: (context, state) => HomePage(),
+              routes: [
+                // 홈에서 파생되는 상세 페이지들
+                GoRoute(
+                  path: '/post/:postId',
+                  name: 'post_detail',
+                  builder: (context, state) {
+                    final postId = state.pathParameters['postId']!;
+                    return PostDetailPage(postId: postId);
+                  },
+                ),
+              ],
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+
+        // 글쓰기 탭
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/write',
+              name: 'write',
+              builder: (context, state) => WritePage(),
+            ),
+          ],
+        ),
+
+        // 프로필 탭
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/profile',
+              name: 'profile',
+              builder: (context, state) => ProfilePage(),
+              routes: [
+                // 프로필에서 파생되는 페이지들
+                GoRoute(
+                  path: '/edit',
+                  name: 'profile_edit',
+                  builder: (context, state) => ProfileEditPage(),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    ),
+  ],
+);
---
+++ lib/write/presentation/widgets/bottom_navigation.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class BottomNavigation extends StatelessWidget {
+  final StatefulNavigationShell navigationShell;
+
+  const BottomNavigation({super.key, required this.navigationShell});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      body: navigationShell,
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          height: 80,
+          decoration: BoxDecoration(
+            color: theme.colorScheme.surface,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withValues(alpha: 0.1),
+                blurRadius: 10,
+                offset: const Offset(0, -2),
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _buildNavItem(
+                context,
+                iconPath: 'assets/icons/home_grey.png',
+                activeIconPath: 'assets/icons/home_orange.png',
+                index: 0,
+              ),
+              _buildNavItem(
+                context,
+                iconPath: 'assets/icons/plus_grey.png',
+                activeIconPath: 'assets/icons/plus_orange.png',
+                index: 1,
+              ),
+              _buildNavItem(
+                context,
+                iconPath: 'assets/icons/profile_grey.png',
+                activeIconPath: 'assets/icons/profile_orange.png',
+                index: 2,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavItem(
+    BuildContext context, {
+    required String iconPath,
+    required String activeIconPath,
+    required int index,
+  }) {
+    final isActive = navigationShell.currentIndex == index;
+    final theme = Theme.of(context);
+
+    return GestureDetector(
+      onTap: () => navigationShell.goBranch(index),
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              isActive ? activeIconPath : iconPath,
+              width: 24,
+              height: 24,
+              color: isActive ? theme.colorScheme.primary : Colors.grey,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
